c64b59172cd8984911f9bb83a51404cadc761ced
Your Name <you@example.com>
2022-02-13 18:01:00 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,27 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(props){
+         super(props)
+        this.realativeList = [
+            {name:puhkar ,location :delhi},
+            {name:Ravi ,location:pune},
+            {name:Rhul , location:up},
+            {name:Gaja , location:guj}
+        ]
+    }
+   
+   
     render() {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+            <ol>
+               {this.realativeList.map((relativeName,index)=>{
+                  const key = "location" + index
+                  return <li key={key}>{relativeName.name}</li>
+               })}
+            </ol>
             </div>
         )
     }
