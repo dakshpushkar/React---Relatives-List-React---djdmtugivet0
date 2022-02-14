182c29315406c6978fbf97e928123e9adf2fad5c
Your Name <you@example.com>
2022-02-14 13:39:43 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,10 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.realativeList = [
-          { name: "puhkar", location: "delhi" },
-          { name: "Ravi", location: "pune" },
-          { name: "Rhul", location: "up" },
-          { name: "Gaja", location: "guj" }
+          { name: "puhkar", key:" relativeListItem1", location: "delhi" },
+          { name: "Ravi",key:" relativeListItem2", location: "pune" },
+          { name: "Rhul", key:" relativeListItem3",location: "up" },
+          { name: "Gaja", key:" relativeListItem4",location: "guj" }
         ];
       }
    
@@ -16,10 +16,10 @@ class App extends Component {
 
         return(
             <div id="main">
-            <ol>
+            <ol key={this.realativeList.key}>
                {this.realativeList.map((relativeName,index)=>{
-                  const key = "location" + index
-                  return <li key={key}>{relativeName.name}</li>
+                  {/* const key = "location" + index */}
+                  return <li key={relativeName.key}>{relativeName.name}</li>
                })}
             </ol>
             </div>
